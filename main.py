35308535fd75379b6222de1eb4857b48d8dd6b5a
Wenfan <49372426+WenfanJ@users.noreply.github.com>
2020-10-06 19:26:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import json
+import model
+
+def main(to_train, log_dir, config_filename, checkpoint_dir, skip):
+    """
+    :param to_train: Specify whether it is training or testing. 1: training; 2:
+     resuming from latest checkpoint; 0: testing.
+    :param log_dir: The root dir to save checkpoints and imgs. The actual dir
+    is the root dir appended by the folder with the name timestamp.
+    :param config_filename: The configuration file.
+    :param checkpoint_dir: The directory that saves the latest checkpoint. It
+    only takes effect when to_train == 2.
+    :param skip: A boolean indicating whether to add skip connection between
+    input and output.
+    """
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+
+    with open(config_filename) as config_file:
+        config = json.load(config_file)
+
+    lambda_a = float(config['_LAMBDA_A']) if '_LAMBDA_A' in config else 10.0
+    lambda_b = float(config['_LAMBDA_B']) if '_LAMBDA_B' in config else 10.0
+    pool_size = int(config['pool_size']) if 'pool_size' in config else 50
+
+    to_restore = (to_train == 2)
+    base_lr = float(config['base_lr']) if 'base_lr' in config else 0.0002
+    max_step = int(config['max_step']) if 'max_step' in config else 200
+    network_version = str(config['network_version'])
+    dataset_name = str(config['dataset_name'])
+    do_flipping = bool(config['do_flipping'])
+
+    cyclegan_model = model.CycleGAN(pool_size, lambda_a, lambda_b, log_dir,
+                              to_restore, base_lr, max_step, network_version,
+                              dataset_name, checkpoint_dir, do_flipping, skip)
+
+    if to_train > 0:
+        cyclegan_model.train()
+    else:
+        cyclegan_model.test()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ model.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import tensorflow as tf
+import json
+import numpy as np
+import os
+import random
+import click
+from scipy.misc import imsave
+import utils
+import cyclegan_datasets
+import discriminator
+import generator
+import data_loader, loss
+
+class CycleGAN:
+    def __init__(self, 
+                 pool_size,
+                 lambda1=10,
+                 lambda2=10,
+                 output_root_dir,
+                 to_restore,
+                 _base_lr,
+                 max_step, 
+                 dataset_name, 
+                 checkpoint_dir, 
+                 do_flipping, 
+                 skip
+                ):
+        
+        self._pool_size = pool_size
+        self._size_before_crop = 286
+        
+        self._output_dir = os.path.join(output_root_dir, current_time)
+        self._images_dir = os.path.join(self._output_dir, 'imgs')
+        
+        self._base_lr = _base_lr
+        self._max_step = max_step
+        
+        self._num_imgs_to_save = 20
+        self._to_restore = to_restore
+        
+        self._network_version = network_version
+        self._dataset_name = dataset_name
+        self._checkpoint_dir = checkpoint_dir
+        self._do_flipping = do_flipping
+        self._skip = skip
+    
+        ## Define the hyperparameters
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
+        
+        
+        self.fake_x_image = np.zeros(
+            (self._pool_size, 1, utils.IMG_HEIGHT, utils.IMG_WIDTH,
+             utils.IMG_CHANNELS)
+        )
+        self.fake_y_image = np.zeros(
+            (self._pool_size, 1, utils.IMG_HEIGHT, utils.IMG_WIDTH,
+             utils.IMG_CHANNELS)
+        )
+        
+    def model(self):
+        self.input_x = tf.placeholder(
+            tf.float32, shape=[
+                1,
+                model.IMG_HEIGHT,
+                model.IMG_WIDTH,
+                model.IMG_CHANNELS
+            ], name="input_X")
+        self.input_y = tf.placeholder(
+            tf.float32, shape=[
+                1,
+                model.IMG_HEIGHT,
+                model.IMG_WIDTH,
+                model.IMG_CHANNELS
+            ], name="input_Y")
+                       
+        ## Define a placeholder fed with fake x/fake y
+        self.fake_pool_x = tf.placeholder(
+            tf.float32, shape=[
+                None,
+                utils.IMG_HEIGHT,
+                utils.IMG_WIDTH,
+                utils.IMG_CHANNELS
+            ], name="fake_pool_A")
+        
+        self.fake_pool_y = tf.placeholder(
+            tf.float32, shape=[
+                None,
+                utils.IMG_HEIGHT,
+                utils.IMG_WIDTH,
+                utils.IMG_CHANNELS
+            ], name="fake_pool_B")
+        
+        self.global_step = tf.Variable(1, name="global_step")
+
+        self.num_fake_inputs = 0
+
+        self.learning_rate = tf.placeholder(tf.float32, shape=[], name="lr")
+
+        self.images_x = self.input_x
+        self.images_y = self.input_y
+        
+        self.G = generator.Generator(name="G_X", skip=skip)
+        self.F = generator.Generator(name="G_Y", skip=skip)
+        self.D_X = discriminator.Discriminator('D_X')
+        self.D_Y = discriminator.Discriminator('D_Y')
+
+        self.prob_real_x_is_real = self.D(images_x)
+        self.prob_real_y_is_real = self.D(images_y)
+        
+        self.fake_images_y = self.G(images_x)
+        self.fake_images_x = self.F(images_y)
+            
+        self.prob_fake_x_is_real = self.D_X(fake_x)
+        self.prob_fake_y_is_real = self.D_Y(fake_y)
+
+        self.cycle_images_x = self.G(fake_y, skip=skip)
+        self.cycle_images_y = self.F(fake_x, skip=skip)
+
+        self.prob_fake_pool_x_is_real = self.D(self.fake_pool_x)
+        self.prob_fake_pool_y_is_real = self.D(self.fake_pool_y)
+        
+        
+    def compute_losses(self):    
+        
+        G_cycle_loss = self.lambda1 * loss.cycle_consistency_loss(self.input_x, self.cycle_images_x)
+        F_cycle_loss = self.lambda2 * loss.cycle_consistency_loss(self.input_y, self.cycle_images_y)
+        
+        G_gan_loss = loss.loss_generator(self.prob_fake_x_is_real)
+        F_gan_loss = loss.loss_generator(self.prob_fake_y_is_real)
+        
+        G_loss = G_cycle_loss + F_cycle_loss + F_gan_loss
+        F_loss = F_cycle_loss + G_cycle_loss + G_gan_loss
+            
+        D_Y_loss = loss.loss_discriminator(self.prob_real_x_is_real, self.prob_fake_pool_x_is_real)
+        D_Y_loss = loss.loss_discriminator(self.prob_real_y_is_real, self.prob_fake_pool_y_is_real)
+        
+        
+        optimizer = tf.train.AdamOptimizer(self.learning_rate, self.beta1)
+        
+        self.model_vars = tf.trainable_variables()
+        
+        d_X_vars = [var for var in self.model_vars if 'D_X' in var.name]
+        g_X_vars = [var for var in self.model_vars if 'G_X' in var.name]
+        d_Y_vars = [var for var in self.model_vars if 'D_Y' in var.name]
+        g_Y_vars = [var for var in self.model_vars if 'G_Y' in var.name]
+
+        self.d_X_trainer = optimizer.minimize(D_X_loss, var_list=d_X_vars)
+        self.d_X_trainer = optimizer.minimize(D_Y_loss, var_list=d_Y_vars)
+        self.g_Y_trainer = optimizer.minimize(G_loss, var_list=g_X_vars)
+        self.g_Y_trainer = optimizer.minimize(F_loss, var_list=g_Y_vars)
+        
+        for var in self.model_vars:
+            print(var.name)
+            
+        # Summary variables for tensorboard
+        self.g_A_loss_summ = tf.summary.scalar("g_A_loss", g_loss_A)
+        self.g_B_loss_summ = tf.summary.scalar("g_B_loss", g_loss_B)
+        self.d_A_loss_summ = tf.summary.scalar("d_A_loss", d_loss_A)
+        self.d_B_loss_summ = tf.summary.scalar("d_B_loss", d_loss_B)
+
+        
+    def save_images(self, sess, epoch):
+         """
+        Saves input and output images.
+        :param sess: The session.
+        :param epoch: Currnt epoch.
+        """
+        if not os.path.exists(self._images_dir):
+            os.makedirs(self._images_dir)
+
+        names = ['inputX_', 'inputY_', 'fakeX_',
+                 'fakeY_', 'cycX_', 'cycY_']
+
+        with open(os.path.join(
+                self._output_dir, 'epoch_' + str(epoch) + '.html'), 'w') as v_html:
+            for i in range(0, self._num_imgs_to_save):
+                print("Saving image {}/{}".format(i, self._num_imgs_to_save))
+                inputs = sess.run(self.inputs)
+                fake_X_temp, fake_Y_temp, cyc_X_temp, cyc_Y_temp = sess.run([
+                    self.fake_images_x,
+                    self.fake_images_y,
+                    self.cycle_images_x,
+                    self.cycle_images_y], 
+                    feed_dict={
+                    self.input_x: inputs['images_i'],
+                    self.input_y: inputs['images_j']
+                })
+
+                tensors = [inputs['images_i'], inputs['images_j'],
+                           fake_Y_temp, fake_X_temp, cyc_X_temp, cyc_Y_temp]
+
+                for name, tensor in zip(names, tensors):
+                    image_name = name + str(epoch) + "_" + str(i) + ".jpg"
+                    imsave(os.path.join(self._images_dir, image_name),
+                           ((tensor[0] + 1) * 127.5).astype(np.uint8))
+                    v_html.write(
+                        "<img src=\"" +
+                        os.path.join('imgs', image_name) + "\">"
+                    )
+                v_html.write("<br>")
+        
+     
+    def fake_image_pool(self, num_fakes, fake, fake_pool):
+        if num_fakes < self._pool_size:
+            fake_pool[num_fakes] = fake
+            return fake
+        else:
+            p = random.random()
+            if p > 0.5:
+                random_id = random.randint(0, self._pool_size - 1)
+                temp = fake_pool[random_id]
+                fake_pool[random_id] = fake
+                return temp
+            else:
+                return fake
+    
+    
+    def train(self):
+        """Training Function."""
+        # Load Dataset from the dataset folder
+        # Need to be modified
+        self.inputs = data_loader.load_data(
+            self._dataset_name, self._size_before_crop,
+            True, self._do_flipping)
+
+        # Build the network
+        self.model()
+
+        # Loss function calculations
+        self.compute_losses()
+
+        # Initializing the global variables
+        init = (tf.global_variables_initializer(),
+                tf.local_variables_initializer())
+        saver = tf.train.Saver()
+
+        max_images = cyclegan_datasets.DATASET_TO_SIZES[self._dataset_name]
+
+        with tf.Session() as sess:
+            sess.run(init)
+
+            # Restore the model to run the model from last checkpoint
+            if self._to_restore:
+                chkpt_fname = tf.train.latest_checkpoint(self._checkpoint_dir)
+                saver.restore(sess, chkpt_fname)
+
+            writer = tf.summary.FileWriter(self._output_dir)
+
+            if not os.path.exists(self._output_dir):
+                os.makedirs(self._output_dir)
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+
+            # Training Loop
+            for epoch in range(sess.run(self.global_step), self._max_step):
+                print("In the epoch ", epoch)
+                saver.save(sess, os.path.join(self._output_dir, "cyclegan"), global_step=epoch)
+
+                # Dealing with the learning rate as per the epoch number
+                if epoch < 100:
+                    curr_lr = self._base_lr
+                else:
+                    curr_lr = self._base_lr - self._base_lr * (epoch - 100) / 100
+
+                self.save_images(sess, epoch)
+
+                for i in range(0, max_images):
+                    print("Processing batch {}/{}".format(i, max_images))
+
+                    inputs = sess.run(self.inputs)
+
+                    # Optimizing the G_X network
+                    _, fake_Y_temp, summary_str = sess.run(
+                        [self.G_X_trainer,
+                         self.fake_images_y,
+                         self.G_X_loss_summ],
+                        feed_dict={
+                            self.input_x:
+                                inputs['images_i'],
+                            self.input_y:
+                                inputs['images_j'],
+                            self.learning_rate: curr_lr
+                        }
+                    )
+                    writer.add_summary(summary_str, epoch * max_images + i)
+
+                    fake_Y_temp1 = self.fake_image_pool(
+                        self.num_fake_inputs, fake_Y_temp, self.fake_images_Y)
+
+                    # Optimizing the D_Y network
+                    _, summary_str = sess.run(
+                        [self.D_Y_trainer, self.D_Y_loss_summ],
+                        feed_dict={
+                            self.input_x:
+                                inputs['images_i'],
+                            self.input_y:
+                                inputs['images_j'],
+                            self.learning_rate: curr_lr,
+                            self.fake_pool_Y: fake_Y_temp1
+                        }
+                    )
+                    writer.add_summary(summary_str, epoch * max_images + i)
+
+                    # Optimizing the G_Y network
+                    _, fake_X_temp, summary_str = sess.run(
+                        [self.G_Y_trainer,
+                         self.fake_images_x,
+                         self.G_Y_loss_summ],
+                        feed_dict={
+                            self.input_x:
+                                inputs['images_i'],
+                            self.input_y:
+                                inputs['images_j'],
+                            self.learning_rate: curr_lr
+                        }
+                    )
+                    writer.add_summary(summary_str, epoch * max_images + i)
+
+                    fake_X_temp1 = self.fake_image_pool(
+                        self.num_fake_inputs, fake_X_temp, self.fake_images_x)
+
+                    # Optimizing the D_X network
+                    _, summary_str = sess.run(
+                        [self.D_X_trainer, self.D_X_loss_summ],
+                        feed_dict={
+                            self.input_x:
+                                inputs['images_i'],
+                            self.input_y:
+                                inputs['images_j'],
+                            self.learning_rate: curr_lr,
+                            self.fake_pool_X: fake_X_temp1
+                        }
+                    )
+                    writer.add_summary(summary_str, epoch * max_images + i)
+
+                    writer.flush()
+                    self.num_fake_inputs += 1
+
+                sess.run(tf.assign(self.global_step, epoch + 1))
+
+            coord.request_stop()
+            coord.join(threads)
+            writer.add_graph(sess.graph)
+            
+    def test(self):
+        """Test Function."""
+        print("Testing the results")
+
+        self.inputs = data_loader.load_data(
+            self._dataset_name, self._size_before_crop,
+            False, self._do_flipping)
+
+        self.model_setup()
+        saver = tf.train.Saver()
+        init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+            sess.run(init)
+
+            chkpt_fname = tf.train.latest_checkpoint(self._checkpoint_dir)
+            saver.restore(sess, chkpt_fname)
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+
+            self._num_imgs_to_save = cyclegan_datasets.DATASET_TO_SIZES[
+                self._dataset_name]
+            self.save_images(sess, 0)
+
+            coord.request_stop()
+            coord.join(threads)
+
+
+# In[ ]:
+
+
+
+
